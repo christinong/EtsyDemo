3ab3f1cf5fbfe3fb8f0dda728f6ccb666c0881c7
Brad <bradley.allen@live.com>
2016-04-11 18:21:20 -0500

    Add seller page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  resources :listings do
-  resources :reviews, except: [:show, :index]
-  end
+  resources :listings
+  resources :listings
+
   get 'pages/about'
-
   get 'pages/contact'
-
   get 'pages/aboutcontact'
+  get 'seller' => "listings#seller"
+
   root'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
