3bc8d395ec0913081d47372214b61d3ac77e07f9
Brad <bradley.allen@live.com>
2016-04-11 18:04:49 -0500

    add validate users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,6 +3,9 @@ class Listing < ActiveRecord::Base
 	   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/,
 	   					 :storage => :dropbox,
   						 :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+		validates :name, :description, :price, presence: true
+		validates :price, numericality: {greater_than:0}
+		validates_attachment_presence :image
 
-						 has_many :reviews, dependent: :destroy
+		belongs_to :user
 end
